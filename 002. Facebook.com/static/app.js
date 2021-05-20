0b5cc85a542df147ce6e44dbccca95f510f72f02
tamhoangweb <tamhoangweb@gmail.com>
2021-05-20 23:02:05 +0700

    Lỗi darkmode

---
+++ 002. Facebook.com/static/app.js
@@ -0,0 +1,6 @@
+document.querySelector("#dark-mode-toogle").onclick = () => {
+  document.documentElement.classList.toggle("dark");
+};
+document.querySelector("#dark-mode-toggle-mb").onclick = () => {
+  document.documentElement.classList.toggle("dark");
+};
